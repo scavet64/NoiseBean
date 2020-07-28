9f71301b4a1b954f8efb6d103e929ad55867f10c
Vincent Scavetta <vstro24@gmail.com>
2020-07-28 19:41:07 -0400

    Adding comments

--- src/main/java/com/scavettapps/noisebean/sounds/SoundFileService.java
+++ src/main/java/com/scavettapps/noisebean/sounds/SoundFileService.java
@@ -16,6 +16,7 @@
 package com.scavettapps.noisebean.sounds;
 
 import com.google.common.io.Files;
+
 import java.io.File;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
@@ -24,6 +25,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message.Attachment;
 import org.apache.commons.io.FileUtils;
@@ -31,44 +33,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- *
  * @author Vincent Scavetta.
  */
 @Service
 @Slf4j
 public class SoundFileService {
-   
+
    private final static String SOUND_FILE_PATH_STRING = "./AudioFiles/";
    private final static File SOUND_FILE_PATH = new File(SOUND_FILE_PATH_STRING);
-   
+
    private final SoundFileRepository soundFileRepository;
 
+   /**
+    * Autowired Constructor for the SoundFileService
+    * @param soundFileRepository The sound file repository for CRUD functionality.
+    */
    @Autowired
    public SoundFileService(SoundFileRepository soundFileRepository) {
       this.soundFileRepository = soundFileRepository;
-      if(!SOUND_FILE_PATH.exists()) {
+      if (!SOUND_FILE_PATH.exists()) {
          SOUND_FILE_PATH.mkdir();
       }
    }
-   
+
+   /**
+    * Get the sound file object that has this ID.
+    *
+    * @param id The ID of the sound that should be retrieved.
+    * @return The sound file that has this ID.
+    * @throws SoundFileNotFoundException If the sound file does not exist
+    */
    public SoundFile getSoundFile(Long id) throws SoundFileNotFoundException {
       return this.soundFileRepository.findById(id).orElseThrow(
-          () -> new SoundFileNotFoundException()
-      );
-   }
-   
-   public SoundFile getSoundFile(String soundFileName) throws SoundFileNotFoundException {
-      return this.soundFileRepository.findBySoundFileName(soundFileName).orElseThrow(
-          () -> new SoundFileNotFoundException()
+         () -> new SoundFileNotFoundException()
       );
    }
 
+   /**
+    * Get the sound file object that has this name.
+    *
+    * @param soundFileName The name of the sound that should be received
+    * @return The sound file that has this name
+    * @throws SoundFileNotFoundException If the sound file did not exist
+    */
+   public SoundFile getSoundFile(String soundFileName) throws SoundFileNotFoundException {
+      return this.soundFileRepository.findBySoundFileName(soundFileName).orElseThrow(
+         () -> new SoundFileNotFoundException()
+      );
+   }
+
+   /**
+    * Does a sound file with this name exist in the database?
+    *
+    * @param soundFileName The name to check for.
+    * @return true if the name exists, false otherwise
+    */
    public boolean doesSoundExist(String soundFileName) {
       return this.soundFileRepository.findBySoundFileName(soundFileName).isPresent();
    }
-   
+
+   /**
+    * Save a sound file.
+    *
+    * @param soundFileName The name of the sound file that should be saved.
+    * @param attachment The file attached to the discord message.
+    * @return The persisted sound file object
+    * @throws SoundFileDownloadException If there was a problem downloading the file to the filesystem.
+    */
    public SoundFile saveSoundFile(String soundFileName, Attachment attachment) throws SoundFileDownloadException {
-      
+
       try {
          File downloadedFile = new File(SOUND_FILE_PATH_STRING + attachment.getFileName());
 
@@ -79,25 +112,32 @@ public class SoundFileService {
 
          CompletableFuture<File> future = attachment.downloadToFile(downloadedFile);
          File completedFile = future.get(1, TimeUnit.MINUTES);
-         
+
          SoundFile newFile = new SoundFile(soundFileName, completedFile.getAbsolutePath());
-         
+
          return this.soundFileRepository.save(newFile);
       } catch (TimeoutException | ExecutionException | InterruptedException ex) {
          Logger.getLogger(SoundFileService.class.getName()).log(Level.SEVERE, null, ex);
          throw new SoundFileDownloadException();
       }
    }
-   
+
    public Collection<SoundFile> getAllSoundFiles() {
       return this.soundFileRepository.findAll();
    }
-   
+
+   /**
+    * Delete a sound file.
+    *
+    * @param soundFileName The sound file name that should be deleted
+    * @return True if the deletion was successful and that the file was removed from the filesystem
+    * @throws SoundFileNotFoundException If the sound file was not found
+    */
    public boolean deleteSoundFile(String soundFileName) throws SoundFileNotFoundException {
       SoundFile sound = this.soundFileRepository.findBySoundFileName(soundFileName).orElseThrow(
-          () -> new SoundFileNotFoundException()
+         SoundFileNotFoundException::new
       );
-      
+
       boolean deleted = true;
       File soundFilePath = new File(sound.getFilePath());
       if (soundFilePath.exists()) {
@@ -106,16 +146,26 @@ public class SoundFileService {
       this.soundFileRepository.delete(sound);
       return deleted;
    }
-   
+
+   /**
+    * Rename a sound file to a new name
+    *
+    * @param soundFileName The sound file to update.
+    * @param newSoundName  The new sound file name.
+    * @return The updated sound file object.
+    * @throws SoundFileNotFoundException If the sound file did not exist
+    */
    public SoundFile renameSoundFile(String soundFileName, String newSoundName) throws SoundFileNotFoundException {
       SoundFile sound = this.soundFileRepository.findBySoundFileName(soundFileName).orElseThrow(
-          () -> new SoundFileNotFoundException()
+         SoundFileNotFoundException::new
       );
-      
+
       this.soundFileRepository.findBySoundFileName(soundFileName).ifPresent(
-          s ->  { throw new SoundFileAlreadyExistsException(s.getSoundFileName()); }
+         s -> {
+            throw new SoundFileAlreadyExistsException(s.getSoundFileName());
+         }
       );
-      
+
       sound.setSoundFileName(newSoundName);
       return this.soundFileRepository.save(sound);
    }
